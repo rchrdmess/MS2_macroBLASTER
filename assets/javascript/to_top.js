5673b1d75fd405858edd3e80b89340ca383a1e00
rchrdmess <126493151+rchrdmess@users.noreply.github.com>
2023-11-21 23:41:26 +0000

    Update to_top.js
    Adding comments.

--- assets/javascript/to_top.js
+++ assets/javascript/to_top.js
@@ -1,9 +1,13 @@
+// The below comment is used on every page to tell the browser to assume standard browser properties. Advised to use by Chatgpt to prevent JSLint from thinking certain commands are variables
+
 /*global document*/
 
-// Function to return to top of page when clicked. Courtesy of W3 Schools
+// The 'toTop' element is defined and linked to the toTopFunction function, which triggers when toTop element is clicked
 
 let toTop = document.getElementById("toTop");
 toTop.addEventListener("click", toTopFunction);
+
+// Function to return to top of page when clicked. Courtesy of W3 Schools
 
 function toTopFunction() {
     document.body.scrollTop = 0;
